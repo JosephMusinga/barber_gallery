55a739d0f54fd59400e7a875b615daf66218b0d3
Jos <98326351+JosephMusinga@users.noreply.github.com>
2023-07-27 16:29:53 +0200

    Connect node server and React frontend

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Camera from './components/Camera';
 import Home from './components/Home';
 import Gallery from './components/Gallery';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const[message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+    
+  }, []);
   
   return (
 
@@ -12,6 +21,8 @@ function App() {
       <div className='header'>
         <h1>Barber Next Cut</h1>
       </div>
+        <h1 style={{color:'red'}}>{message}</h1>
+
       <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<Home />} />
