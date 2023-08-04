438177ebe89c051808ea9fa8b206a2563a0e13fa
Jos <98326351+JosephMusinga@users.noreply.github.com>
2023-08-04 10:35:41 +0200

    convert to express app

--- server/server.js
+++ server/server.js
@@ -12,28 +12,3 @@
 // app.listen(8000, () => {
 //   console.log(`Server is running on port 8000.`);
 // });
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = 'C:/Users/Lenovo//Pictures';
-      var newpath = oldpath  + '/App.js';
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
-}).listen(8080);
