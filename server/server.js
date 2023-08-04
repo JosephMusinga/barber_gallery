34ddef6833186e221206e13fb40864a3370aeb41
Jos <98326351+JosephMusinga@users.noreply.github.com>
2023-08-04 09:43:27 +0200

    node tutorial

--- server/server.js
+++ server/server.js
@@ -13,19 +13,27 @@
 //   console.log(`Server is running on port 8000.`);
 // });
 var http = require('http');
+var formidable = require('formidable');
 var fs = require('fs');
-var url = require('url');
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data){
-    if (err){
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = 'C:/Users/Lenovo//Pictures';
+      var newpath = oldpath  + '/App.js';
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
     return res.end();
-  });
+  }
 }).listen(8080);
